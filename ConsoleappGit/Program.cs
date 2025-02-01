ab944e1b97b625bb939f3542cc26cccd36caf31a
gianluca.vernocchi <gianluca.vernocchi@PC2224BIS>
2025-02-01 08:51:08 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleappGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleappGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
